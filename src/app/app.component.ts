8d16dfea47da4a147f3acc46809405f0aa55d2b8
Ahsan Alamgir <ahsanalamgir14@gmail.com>
2024-08-09 05:59:56 +0500

    Added flowbite tailwind components in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { FlowbiteService } from './services/flowbite.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cliente';
 
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private flowbiteService: FlowbiteService) {
     this.translate.setDefaultLang('en');
     this.translate.use('en');
   }
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite(flowbite => {
+      // Your custom code here
+      console.log('Flowbite loaded', flowbite);
+    });
+  }
+
 
   switchLanguage(language: string) {
     this.translate.use(language);
